b5665e147f628fbf8a4779e455aa1d9cc72e23f8
Yasmine-Zahreddine <zahreddineyasmine@gmail.com>
2024-12-25 18:08:46 +0200

    added navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,10 @@
+import Navbar from "./components/navbar/Navbar";
+import "./App.css"
+
 function App() {
-  return <>
-     
-  </>;
+  return <div className="App">
+     <Navbar/>
+  </div>;
 }
 
 export default App;
---
+++ frontend/src/components/navbar/Navbar.jsx
@@ -0,0 +1,58 @@
+import React, {useState} from 'react'
+import './navbar.css'
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
+import logo from "../../assets/logos/logo_lighttheme_thinklink.png"
+import SearchIcon from '../../assets/search.svg';
+
+const Menu = ()=> (
+  <>
+    <p><a href="#home">Home</a></p>
+    <p><a href="#my_learning">My Learning</a></p>
+    <p><a href="#support">Support</a></p>
+  </>
+)
+
+const Navbar = () => {
+  const[toggleMenu, setToggleMenu] = useState(false)
+  return (
+    <div className='navbar'>
+      <div className='navbar-links'>
+        <div className='navbar-links_logo'>
+          <img src={logo} alt='logo'/>
+        </div>
+        <div className='navbar-links_container'>
+          <Menu/>
+        </div>
+      </div>
+      <div className='navbar-search'>
+          <input
+            placeholder='Search'
+          />
+          <img 
+            src={SearchIcon}
+            alt='search'
+          />
+      </div>
+      <div className='navbar-sign'>
+        <p>Sign in</p>
+        <button type='button'>Sign up</button>
+      </div>
+      <div className='navbar-menu'>
+        {toggleMenu ? <RiCloseLine color='#fff' size= {27} onClick={()=>{setToggleMenu(false)}}/> : <RiMenu3Line color='#fff' size={27} onClick={()=>setToggleMenu(true)}/>}
+        {toggleMenu && (
+          <div className='navbar-menu_container scale-up-center'>
+            <div className='navbar-menu_container-links'>
+              <Menu/>
+              <div className='navbar-menu_container-links-sign'>
+                <p>Sign in</p>
+                <button type='button'>Sign up</button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
