deeaa3a4becfb6d79fa2993bf5d3360fb5ae057c
Yasmine-Zahreddine <zahreddineyasmine@gmail.com>
2025-01-09 21:51:04 +0200

    edited the message returned in case of exception

--- frontend/api/signin.js
+++ frontend/api/signin.js
@@ -8,6 +8,6 @@ export const signin = async (formData) => {
         const response = await axios.post(`${API_BASE_URL}/api/signin`, formData);
         return response.data;
     } catch (error) {
-        throw error.response ? error.response.data : { message: "Network error" };
+        throw error.response ? error.response.data : { message: "Something went wrong, please try again!" };
     }
 }; 
--- frontend/api/signup.js
+++ frontend/api/signup.js
@@ -10,6 +10,6 @@ export const signup = async (formData) => {
         const response = await axios.post(`${API_BASE_URL}/api/signup`, formData);
         return response.data;
     } catch (error) {
-        throw error.response ? error.response.data : { message: "Network error" };
+        throw error.response ? error.response.data : { message: "Something went wrong, please try again!" };
     }
 };
