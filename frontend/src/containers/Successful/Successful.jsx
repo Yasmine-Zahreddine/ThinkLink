b1663dfa8e60ac66e4d12b137ade2b0eb3ef5d22
Yasmine-Zahreddine <zahreddineyasmine@gmail.com>
2025-01-09 21:21:27 +0200

    used card component in successful container

--- frontend/src/containers/Successful/Successful.jsx
+++ frontend/src/containers/Successful/Successful.jsx
@@ -1,6 +1,6 @@
-import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./successful.css";
+import Card from "../../components/card/Card";
 
 const Successful = () => {
   const navigate = useNavigate(); // Use the navigate hook for navigation
@@ -11,29 +11,24 @@ const Successful = () => {
 
   return (
     <div className="signip_successful_container">
- <div className="animated-tick">
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 100 100"
-    className="tick-svg"
-  >
-    <circle className="tick-circle" cx="50" cy="50" r="48" fill="none" />
-    <path
-  className="tick-path"
-  d="M20 50 L45 75 L80 30"
-  fill="none"
-  stroke="#4caf50"
-  strokeWidth="6"
+      <div className="animated-tick">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 100 100"
+          className="tick-svg"
+        >
+          <circle className="tick-circle" cx="50" cy="50" r="48" fill="none" />
+          <path
+            className="tick-path"
+            d="M20 50 L45 75 L80 30"
+            fill="none"
+            stroke="#4caf50"
+            strokeWidth="6"
+          />
+        </svg>
+      </div>
 
-/>
-
-  </svg>
-</div>
-
-
-      <h1 className="gradient_text">Sign-Up Successful</h1>
-      <h4>Welcome aboard! We're excited to have you join us on this journey.</h4>
-      <h4>You’re all set! Explore your dashboard to get started.</h4>
+      <Card title="Account Created Successfully!" content="Welcome aboard! We&apos;re excited to have you join us on this journey."/>
       <button onClick={handleDashboardClick} className="button_home">
         Go to Home
       </button>
