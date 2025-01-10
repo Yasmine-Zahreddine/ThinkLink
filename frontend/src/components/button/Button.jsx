d2e90287f2b438514363fbaba174125d65168816
Yasmine-Zahreddine <zahreddineyasmine@gmail.com>
2025-01-10 19:50:30 +0200

    fixed the on hover color of the button

--- frontend/src/components/button/Button.jsx
+++ frontend/src/components/button/Button.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
 import "./button.css"
+import PropTypes from 'prop-types'
+
 const Button = (props) => {
   return (
     <button className="buttonS" type="submit">{props.content}</button>
   )
+}
+
+Button.propTypes = {
+  content: PropTypes.string.isRequired
 }
 
 export default Button
