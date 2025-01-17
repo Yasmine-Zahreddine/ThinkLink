4580db3be59df1ab1c5cbedfd90893a8f2589ae5
Majed <majedshmaitlu@gmail.com>
2025-01-17 21:49:36 +0200

    adding animated profile info box that appears when you click on the profile

--- frontend/src/components/pfp/Pfp.jsx
+++ frontend/src/components/pfp/Pfp.jsx
@@ -1,10 +1,54 @@
-import React from 'react'
-import "./pfp.css"
-import user from "../../assets/logos/user.png"
-const Pfp = () => {
-  return (
-    <> <img src={user} alt="user" className="user-pfp"/></>
-  )
-}
+import React, { useState } from 'react';
+import './pfp.css';
+import user from '../../assets/logos/user.png';
+import classNames from 'classnames';
 
-export default Pfp
+const Pfp = () => {
+  const [isClicked, setIsClicked] = useState(false);
+  const [isAnimating, setIsAnimating] = useState(false);
+  const [isAnimatingOut, setIsAnimatingOut] = useState(false);
+
+  const handleClick = () => {
+    if (!isClicked && !isAnimatingOut) {
+      // Opening the profile
+      setIsClicked(true);
+      setIsAnimating(true);
+      setIsAnimatingOut(false);
+      setTimeout(() => {
+        setIsAnimating(false);
+      }, 300);
+    } else if (isClicked && !isAnimating) {
+      // Closing the profile
+      setIsAnimating(false);
+      setIsAnimatingOut(true);
+      setTimeout(() => {
+        setIsClicked(false);
+        setIsAnimatingOut(false);
+      }, 300);
+    }
+  };
+
+  return (
+    <div className="pfp-wrapper">
+      <img
+        src={user}
+        alt="user"
+        className="user-pfp"
+        onClick={handleClick}
+      />
+      {(isClicked || isAnimatingOut) && (
+        <div
+          className={classNames('profile-container', {
+            'animate-up': isAnimating,
+            'animate-down': isAnimatingOut,
+          })}
+        >
+          
+
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Pfp;
